36db5c9384bc7d4a46d555f2dbcaa3a0e6ece137
alazi <alazi>
2019-08-02 20:11:07 -0700

    Make loading more robust

--- CUAControllers.cs
+++ CUAControllers.cs
@@ -3,33 +3,182 @@
     using System;
     using System.Collections;
     using System.Collections.Generic;
-    using System.Linq;
-    using AssetBundles;
     using UnityEngine;
     using UnityEngine.UI;
     using System.Text.RegularExpressions;
+    using SimpleJSON;
 
     class CUAControllers : MVRScript
     {
         protected JSONStorableString jsonNodeRE;
         private JSONStorableBool debug;
         bool haveDoneRestore;
+        bool wasFromReload;
+        private Regex regex;
 
-        protected IEnumerator WaitForAssetBundle()
+        private IEnumerator WaitFor(Func<bool> test, float timeoutSeconds = 3.0f, float delay = 0.2f)
         {
-            List<Renderer> allRenderers = new List<Renderer>();
-
-            float waitForSeconds = 3.0f;
-            float waited = 0f;
-            while (allRenderers.Count < 1 && waited <= waitForSeconds) {
-                containingAtom.reParentObject.GetComponentsInChildren<Renderer>(false, allRenderers);
-                waited += 0.2f;
-                yield return new WaitForSeconds(0.2f);
+            float waited = 0;
+            while (!test() && waited <= timeoutSeconds) {
+                waited += delay;
+                yield return new WaitForSeconds(delay);
             }
-            var item = GetContainingAtom().reParentObject;
-            //printIt(item, "");
+        }
+
+        protected IEnumerator RestoreFromLoad()
+        {
+            yield return WaitFor(() => containingAtom.reParentObject.Find("object/rescaleObject").GetComponentsInChildren<Rigidbody>().Length > 0);
+            foreach (var t in GetControlTargets()) {
+                yield return WaitFor(() => GetAtomById(t.controlName) != null);
+            }
             yield return SyncCO();
         }
+
+        public override void RestoreFromJSON(JSONClass jc, bool restorePhysical = true, bool restoreAppearance = true, JSONArray presetAtoms = null, bool setMissingToDefault = true)
+        {
+            wasFromReload = false;
+            base.RestoreFromJSON(jc, restorePhysical, restoreAppearance, presetAtoms, setMissingToDefault);
+            StartCoroutine(RestoreFromLoad());
+        }
+
+        private void DestroyOldAtoms()
+        {
+            if (insideRestore) return;
+            foreach (var item in SuperController.singleton.GetAtoms()) {
+                if (item.uid.StartsWith(baseName)) {
+                    SuperController.LogMessage($"deleting {item.uid}");
+                    SuperController.singleton.RemoveAtom(item);
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            DestroyOldAtoms();
+        }
+        private IEnumerator MaybeWasReload()
+        {
+            yield return null;
+            if (wasFromReload) {
+                DestroyOldAtoms();
+                yield return SyncCO();
+            }
+        }
+
+        // Use this for initialization
+        public override void Init()
+        {
+            UIStringMessage("Target Nodes (regex):", false);
+            StringTextbox(ref jsonNodeRE, "target nodes re", ".*", _ => { regex = new Regex(jsonNodeRE.val); }, true);
+            BoolCheckbox(ref debug, "Debug", false, _ => Sync(), false);
+
+            CreateButton("Rebuild").button.onClick.AddListener(() => { DestroyOldAtoms(); Sync(); });
+
+            regex = new Regex(jsonNodeRE.val);
+            
+            // do a rebuild if we came from an initial setup or the reload button, but not if we're in scene load
+            wasFromReload = true;
+            StartCoroutine(MaybeWasReload());
+        }
+        const string atomType = "Sphere";
+        const string targetGO = "object/rescaleObject/Sphere";
+        private IEnumerator SetupControlAtom(string name)
+        {
+            var controlAtom = GetAtomById(name);
+            if (controlAtom && controlAtom.type != atomType) {
+                SuperController.singleton.RemoveAtom(controlAtom);
+                controlAtom = null;
+            }
+            if (!controlAtom) {
+                yield return SuperController.singleton.AddAtomByType(atomType, name);
+                controlAtom = SuperController.singleton.GetAtomByUid(name);
+                controlAtom.parentAtom = containingAtom;
+                controlAtom.collisionEnabledJSON.val = false; // don't explode while we're reconnecting on load
+                controlAtom.mainController.enableSelectRoot = true;
+                controlAtom.mainController.InitUI();
+            }
+        }
+
+        private GameObject CreateControlLink(string rbName)
+        {
+            var links = containingAtom.transform.Find("ControlLinks");
+            if (!links) {
+                links = new GameObject("ControlLinks").transform;
+                links.parent = containingAtom.transform;
+            }
+            var name = $"${rbName}Link";
+            var link = links.Find(name)?.gameObject;
+            if (link)
+                return link;
+            link = new GameObject(name);
+            link.transform.parent = links;
+            return link;
+        }
+
+        private IEnumerable<ControlTarget> GetControlTargets()
+        {
+            var rescale = containingAtom.reParentObject.Find("object/rescaleObject");
+            foreach (var rb in rescale.GetComponentsInChildren<Rigidbody>()) {
+                if (!regex.IsMatch(rb.name))
+                    continue;
+                var controlName = $"{baseName}{rb.name}::Control";
+                yield return new ControlTarget() { rb = rb, controlName = controlName };
+            }
+        }
+        private void Sync()
+        {
+            if (insideRestore) return;
+            StartCoroutine(SyncCO());
+        }
+        private string baseName => $"z${containingAtom.uid}::";
+        private IEnumerator SyncCO()
+        {
+            while (containingAtom.transform.Find("ControlLinks"))
+                DestroyImmediate(containingAtom.transform.Find("ControlLinks")?.gameObject);
+
+            foreach (var t in GetControlTargets()) {
+                var rb = t.rb; var controlName = t.controlName;
+                yield return SetupControlAtom(controlName);
+                var controlAtom = GetAtomById(controlName);
+
+                controlAtom.reParentObject.position = rb.position;
+                controlAtom.reParentObject.rotation = rb.rotation;
+
+                var linkGO = CreateControlLink(rb.name);
+                var linkRB = linkGO.AddComponent<Rigidbody>();
+                linkRB.transform.position = rb.transform.position;
+                linkRB.transform.rotation = rb.transform.rotation;
+                linkRB.mass = 0.1f;
+                linkRB.drag = 0;
+                linkRB.angularDrag = 0;
+                linkRB.useGravity = false;
+                linkRB.isKinematic = false;
+
+                var controlGO = controlAtom.reParentObject.Find(targetGO);
+
+                CreateFixedJoint(linkGO, controlAtom.reParentObject.Find("object").GetComponent<Rigidbody>());
+                CreateFixedJoint(linkGO, rb);
+
+                if (debug.val) {
+                    linkGO.AddComponent<DebugComponent>();
+                    controlGO.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+                    controlGO.GetComponent<MeshRenderer>().enabled = true;
+                } else {
+                    Destroy(linkGO.GetComponent<DebugComponent>());
+                    controlGO.GetComponent<MeshRenderer>().enabled = false;
+                }
+            }
+        }
+
+        private void CreateFixedJoint(GameObject linkGO, Rigidbody target)
+        {
+            var joint = linkGO.AddComponent<FixedJoint>();
+            joint.autoConfigureConnectedAnchor = false;
+            joint.connectedBody = target;
+            joint.connectedAnchor = new Vector3();
+        }
+
+        // UI helpers
 
         private void FloatSlider(ref JSONStorableFloat output, string name, float start, JSONStorableFloat.SetFloatCallback callback, float min, float max, bool rhs)
         {
@@ -60,145 +209,17 @@
             output.inputField = input;
         }
 
-        private void DestroyOldAtoms()
+        class ControlTarget
         {
-            if (insideRestore) {
-
-                //return;
-                
-            }
-            SuperController.LogMessage($"destroyold: {insideRestore} {System.Environment.StackTrace}");
-            foreach (var item in SuperController.singleton.GetAtoms()) {
-                if (item.uid.StartsWith(baseName)) {
-                    if (insideRestore) { SuperController.LogMessage($"skipping {item.uid} due to restoring"); continue; }
-                    SuperController.LogMessage($"deleting {item.uid}");
-                    SuperController.singleton.RemoveAtom(item);
-                }
-            }
-        }
-
-        private void OnDestroy()
-        {
-            DestroyOldAtoms();
-        }
-
-        // Use this for initialization
-        public override void Init()
-        {
-
-            SuperController.LogMessage($"Init {insideRestore}");
-            //DestroyOldAtoms();
-            UIStringMessage("Target Nodes (regex):", false);
-            StringTextbox(ref jsonNodeRE, "target nodes re", ".*", _ => { DestroyOldAtoms(); Sync(); }, true);
-            BoolCheckbox(ref debug, "Debug", false, _ => Sync(), false);
-
-
-            StartCoroutine(WaitForAssetBundle());
-
-
-        }
-        const string atomType = "Sphere";
-        const string targetGO = "object/rescaleObject/Sphere";
-        private IEnumerator SetupControlAtom(string name)
-        {
-            var controlAtom = GetAtomById(name);
-            if (controlAtom && controlAtom.type != atomType) {
-                SuperController.singleton.RemoveAtom(controlAtom);
-                controlAtom = null;
-            }
-            if (!controlAtom) {
-                yield return SuperController.singleton.AddAtomByType(atomType, name);
-                controlAtom = SuperController.singleton.GetAtomByUid(name);
-                controlAtom.parentAtom = containingAtom;
-            }
-            yield break;
-        }
-
-        private GameObject CreateControlLink(string rbName)
-        {
-            var links = containingAtom.transform.Find("ControlLinks");
-            if (!links) {
-                links = new GameObject("ControlLinks").transform;
-                links.parent = containingAtom.transform;
-            }
-            var name = $"${rbName}Link";
-            var link = links.Find(name)?.gameObject;
-            if (link)
-                return link;
-            link = new GameObject(name);
-            link.transform.parent = links;
-            return link;
-        }
-
-        private void Sync()
-        {
-            if (insideRestore) return;
-            StartCoroutine(SyncCO());
-        }
-        private string baseName => $"z${containingAtom.uid}::";
-        private IEnumerator SyncCO()
-        {
-            while (containingAtom.transform.Find("ControlLinks"))
-                DestroyImmediate(containingAtom.transform.Find("ControlLinks")?.gameObject);
-
-            SuperController.LogMessage($"SyncCO");
-            //SuperController.LogMessage(printIt(containingAtom.transform, ""));
-            var regex = new Regex(jsonNodeRE.val);
-
-            var rescale = containingAtom.reParentObject.Find("object/rescaleObject");
-            foreach (var rb in rescale.GetComponentsInChildren<Rigidbody>()) {
-                if (!regex.IsMatch(rb.name))
-                    continue;
-                var controlName = $"{baseName}{rb.name}::Control";
-                //SuperController.LogMessage($"setup: ${controlName}");
-                yield return SetupControlAtom(controlName);
-                var controlAtom = GetAtomById(controlName);
-
-
-
-
-                controlAtom.reParentObject.position = rb.position;
-                controlAtom.reParentObject.rotation = rb.rotation;
-
-                var linkGO = CreateControlLink(rb.name);
-                var linkRB = linkGO.AddComponent<Rigidbody>();
-                linkRB.transform.position = rb.transform.position;
-                linkRB.transform.rotation = rb.transform.rotation;
-                linkRB.mass = 0.1f;
-                linkRB.drag = 0;
-                linkRB.angularDrag = 0;
-                linkRB.useGravity = false;
-                linkRB.isKinematic = false;
-
-                var controlGO = controlAtom.reParentObject.Find(targetGO);
-                foreach (var item in controlGO.GetComponents<MonoBehaviour>()) {
-                    item.enabled = false;
-                }
-                controlGO.GetComponent<SphereCollider>().enabled = false;
-
-
-                CreateFixedJoint(linkGO, controlAtom.reParentObject.Find("object").GetComponent<Rigidbody>());
-                CreateFixedJoint(linkGO, rb);
-
-                if (debug.val) {
-                    linkGO.AddComponent<DebugComponent>();
-                    controlGO.localScale = new Vector3(0.05f, 0.05f, 0.05f);
-                    controlGO.GetComponent<MeshRenderer>().enabled = true;
-                } else {
-                    Destroy(linkGO.GetComponent<DebugComponent>());
-                    controlGO.GetComponent<MeshRenderer>().enabled = false;
-                }
-            }
-        }
-
-        private void CreateFixedJoint(GameObject linkGO, Rigidbody target)
-        {
-            var joint = linkGO.AddComponent<FixedJoint>();
-            joint.autoConfigureConnectedAnchor = false;
-            joint.connectedBody = target;
-            joint.connectedAnchor = new Vector3();
+            public Rigidbody rb;
+            public string controlName;
         }
     }
+
+
+
+
+    // Debug helpers
     class DebugComponent : MonoBehaviour
     {
 
